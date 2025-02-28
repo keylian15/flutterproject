43f317b5ac1fb9a140e30798dcd14fb640195f3e
SIAME <siame.romain.scw@gmail.com>
2025-02-28 10:17:17 +0100

    maj des widgets pour faire la liste

--- lib/pages/listItemPage.dart
+++ lib/pages/listItemPage.dart
@@ -1,40 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutterproject/Store/app_store.dart';
-import 'package:flutterproject/widget/block.dart';
-import '../widget/navBar_widget.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ListeItemPage extends ConsumerWidget {
-  const ListeItemPage({super.key});
+import '../block_data.dart';
+
+class BlockWidget extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+
+  BlockWidget({super.key, required BlockData block})
+      : name = block.name ?? "Nom inconnu",
+        imageUrl = block.image ?? "";
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(appStoreProvider);
-
-    final widgets = <Widget>[];
-    state.blocks.forEach((block){
-      widgets.add(Block());
-    });
-
-    print(widgets);
-
-    return Scaffold(
-      body: Stack(
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(10),
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        shadowColor: Colors.grey.withValues(alpha: 0, red: 0, blue: 0),
+        elevation: 5,
+      ),
+      onPressed: () {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("Tu as sélectionné : ${name}"),
+          duration: Duration(seconds: 2),
+        ));
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Positioned.fill(
-            child: Image.asset(
-              "assets/images/background.png",
-              fit: BoxFit.cover,
+          Expanded(
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.contain,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) {
+                  return child;
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                          (loadingProgress.expectedTotalBytes ?? 1)
+                          : null,
+                    ),
+                  );
+                }
+              },
+              errorBuilder: (context, error, stackTrace) {
+
+                return Image.asset("assets/images/placeholder.png",
+                    fit: BoxFit.contain);
+              },
             ),
           ),
-          Center(
-            child: ListView(
-              children: widgets,
-            ),
+          const SizedBox(height: 8),
+          Text(
+            name ?? "Nom inconnu",
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
           ),
         ],
       ),
-      bottomNavigationBar: NavBar(currentIndex: 0),
     );
   }
 }
